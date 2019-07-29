f0a12aa4748632b0dd9efeaa0bd629316be44c79
uusa35 <uusa35@gmail.com>
2019-07-29 11:47:59 +0300

    updates

--- app/components/widgets/VideosWidget.js
+++ app/components/widgets/VideosWidget.js
@@ -1,7 +1,8 @@
 import React, {useContext} from 'react';
 import {I18nManager, ScrollView, StyleSheet, Text, View} from 'react-native';
 import {text, width} from '../../constants';
-import {map, isNull, first} from 'lodash';
+import {appUrlIos} from '../../env';
+import {map, isNull} from 'lodash';
 import PropTypes from 'prop-types';
 import {WebView} from 'react-native-webview';
 import I18n from '../../I18n';
@@ -10,7 +11,8 @@ import {GlobalValuesContext} from '../../redux/GlobalValuesContext';
 
 const VideosWidget = ({videos}) => {
   const {colors} = useContext(GlobalValuesContext);
-  console.log('videos', videos['video_url_one']);
+  // console.log('videos', videos['video_url_one']);
+  // console.log(`${appUrlIos}webview?url=${videos['video_url_one']}`);
   return (
     <View style={{width: '90%', alignSelf: 'center', marginTop: 30}}>
       {!isNull(videos['video_url_one']) ? (
@@ -50,7 +52,7 @@ const VideosWidget = ({videos}) => {
                   marginLeft: 5
                 }}
                 javaScriptEnabled={true}
-                source={{uri: `http://mallr.test/webview?url=${v}`}}
+                source={{uri: `${appUrlIos}webview?url=${v}`}}
               />
             ) : null
           )
